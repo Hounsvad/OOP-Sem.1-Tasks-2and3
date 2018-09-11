7fb5b4785e05f9f7de198fbd74635bf9e2884ae4
Frederik Hounsvad <frederik@hounsvad.dk>
2018-09-11 18:28:26 +0200

    Comletion of 5.5 and 5.3

--- src/oopModul3/Liang5_3.java
+++ src/oopModul3/Liang5_3.java
@@ -9,26 +9,21 @@ package oopModul3;
  *
  * @author Pinnacle F
  */
-//(Conversion from Cº to Fº) Write a program that displays the following table (note that farenheit = celsius * 9/5 + 32):
 public class Liang5_3 {
-
-    public static void Main() {
-        System.out.println("Celsius   Fahrenheit | Fahrenheit   Celsius");
+    public static void Main(){
+        System.out.println("Celsius   Fahrenheit");
 
         double celsius = 0;
-        double fahrenheit = 20;
         double cToF;
-        double fToC;
         
         while (celsius < 100) {
             //Calculating
             cToF = celsius * 1.8 + 32;
-            fToC = (fahrenheit-32)/1.8;
             //Printing
-            System.out.printf("%-10.2f%10.2f | %-10.2f%10.2f%n", celsius, cToF, fahrenheit, fToC);
+            System.out.printf("%-10.2f%10.2f%n", celsius, cToF);
             //Incrementing variables
             celsius += 2;
-            fahrenheit += 5;
         }
     }
+    
 }
--- src/oopModul3/Liang5_5.java
+++ src/oopModul3/Liang5_5.java
@@ -9,9 +9,26 @@ package oopModul3;
  *
  * @author Pinnacle F
  */
+//(Conversion from Cº to Fº) Write a program that displays the following table (note that farenheit = celsius * 9/5 + 32):
 public class Liang5_5 {
-    public static void Main(){
+
+    public static void Main() {
+        System.out.println("Celsius   Fahrenheit | Fahrenheit   Celsius");
+
+        double celsius = 0;
+        double fahrenheit = 20;
+        double cToF;
+        double fToC;
         
+        while (celsius < 100) {
+            //Calculating
+            cToF = celsius * 1.8 + 32;
+            fToC = (fahrenheit-32)/1.8;
+            //Printing
+            System.out.printf("%-10.2f%10.2f | %-10.2f%10.2f%n", celsius, cToF, fahrenheit, fToC);
+            //Incrementing variables
+            celsius += 2;
+            fahrenheit += 5;
+        }
     }
-    
 }
