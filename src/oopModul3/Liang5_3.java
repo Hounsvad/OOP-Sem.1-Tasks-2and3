95d920e35ca1b945a43cd9a6b12e4c77a3b54762
Frederik Hounsvad <frederik@hounsvad.dk>
2018-09-11 15:40:29 +0200

    Completion of 5.3

--- src/oopModul3/Liang5_3.java
+++ src/oopModul3/Liang5_3.java
@@ -13,40 +13,22 @@ package oopModul3;
 public class Liang5_3 {
 
     public static void Main() {
-        System.out.println("Celsius      Fahrenheit | Fahrenheit      Celsius");
+        System.out.println("Celsius   Fahrenheit | Fahrenheit   Celsius");
+
+        double celsius = 0;
+        double fahrenheit = 20;
+        double cToF;
+        double fToC;
         
-        double celsius1;
-        double fahrenheit1;
-        String celsiusString1;
-        String fahrenheitString1;
-        String spacer1;
-        
-        double celsius2;
-        double fahrenheit2 = 20;
-        String celsiusString2;
-        String fahrenheitString2;
-        String spacer2;
-
-        for (int i = 0; i <= 100; i += 2) {
-            spacer1 = "";
-            celsius1 = i;
-            fahrenheit1 = ((celsius1 * 9) / 5) + 32;
-            celsiusString1 = Double.toString(celsius1);
-            fahrenheitString1 = Double.toString(fahrenheit1);
-            for (int j = 0; j < 23 - (celsiusString1.length() + fahrenheitString1.length()); j++) {
-                spacer1 += " ";
-            }
-
-            spacer2 = "";
-            celsius2 = (9 / 5 + 32) / fahrenheit2;
-            celsiusString2 = String.format("%.2f", celsius2);
-            fahrenheitString2 = Double.toString(fahrenheit2);
-            for (int j = 0; j < 23 - (celsiusString2.length() + fahrenheitString2.length()); j++) {
-                spacer2 += " ";
-            }
-            fahrenheit2 += 5;
-
-            System.out.println(celsiusString1 + spacer1 + fahrenheitString1 + " | " + fahrenheitString2 + spacer2 + celsiusString2);
+        while (celsius < 100) {
+            //Calculating
+            cToF = ((celsius * 9) / 5) + 32;
+            fToC = (fahrenheit-32)/1.8;
+            //Printing
+            System.out.printf("%-10.2f%10.2f | %-10.2f%10.2f%n", celsius, cToF, fahrenheit, fToC);
+            //Incrementing variables
+            celsius += 2;
+            fahrenheit += 5;
         }
     }
 }
