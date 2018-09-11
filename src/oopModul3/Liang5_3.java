964c31207d6c700f620ed130d30af901e5aa3b2d
Frederik Hounsvad <frederik@hounsvad.dk>
2018-09-11 09:25:05 +0200

    work on 5.3

--- src/oopModul3/Liang5_3.java
+++ src/oopModul3/Liang5_3.java
@@ -14,9 +14,22 @@ public class Liang5_3 {
 
     public static void Main() {
         System.out.println("Celsius      Fahrenheit");
-//        for (int i = 1; i <= 100; i++) {
-//            
-//        }
-    }
+        System.out.println("Celsius      Fahrenheit".length() + "is the length of the string");
+        double celsius = 0;
+        double fahrenheit = 0;
+        String celsiusString = "";
+        String fahrenheitString = "";
+        String spacer = "";
+        for (int i = 2; i <= 100; i += 2) {
+            celsius = i;
+            fahrenheit = ((celsius * 9) / 5) + 32;
+            celsiusString = Double.toString(celsius);
+            fahrenheitString = Double.toString(fahrenheit);
+            for (int j = 0; i < 23 - (celsiusString.length() + fahrenheitString.length()); j++) {
+                spacer += " ";
+            }
 
+            System.out.println(celsiusString + spacer + fahrenheitString);
+        }
+    }
 }
